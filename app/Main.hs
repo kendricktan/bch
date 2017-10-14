d3925f91010ce5df5606529487666d098810c5a2
Kendrick Tan <kendricktan0814@gmail.com>
2017-10-14 15:28:09 +1000

    mvp with rest api

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,102 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE Rank2Types        #-}
 module Main where
 
-import Lib
+import qualified Control.Monad.State as S
+import           Data.Aeson          hiding (json)
 import qualified Data.ByteString
+import           Data.IORef
+import           Data.Monoid         ((<>))
+import           Data.String         (fromString)
+import           Data.Text           (Text, pack)
+import           GHC.Generics
+import           Lib
+import           Web.Spock.Config
+import           Web.Spock.Core
+
+newtype RunStateT s m = RunStateT { runStateT :: forall a. S.StateT s m a -> m a }
+
+type Blockchain = [Block]
+
+
+-- Adds a transaction
+--
+addTx :: (Monad m) => Transaction -> S.StateT Blockchain m Blockchain
+addTx tx = do
+    bc <- S.get
+    let bh = Prelude.head bc -- Block head
+    let bt = Prelude.tail bc -- Block tail
+    let bh' = blockAddTx bh tx -- Add tx to the head of block
+    let bc' = ([bh'] ++ bt) -- New blockchain
+    S.put bc'
+    return [bh']
+
+
+-- Mines the block head
+--
+mine :: (Monad m) => S.StateT Blockchain m Blockchain
+mine = do
+    bc <- S.get -- Blockchain
+    let bh = Prelude.head bc -- Block head
+    let bt = Prelude.tail bc -- Block tail
+    let mb = mineBlock bh 0 -- Mined block
+    let bh' = Block (index mb) [] "" (hash mb) Nothing -- New block head
+    let bc' = ([bh', mb] ++ bt)
+    S.put bc'
+    return [mb]
+
+
+-- Returns current block head
+--
+current :: (Monad m) => S.StateT Blockchain m Blockchain
+current = do
+    bc <- S.get
+    return $ [Prelude.head bc]
+
+
+-- Gets current blockhash
+--
+getBlockhash :: (Monad m) => String -> S.StateT Blockchain m Blockchain
+getBlockhash h = do
+    bc <- S.get
+    return $ filter (\b -> (hash b) == h) bc
+
+
+-- Gets entire blockchasin
+--
+getBlockchain :: (Monad m) => S.StateT Blockchain m Blockchain
+getBlockchain = S.get >>= return
+
+
+-- StateT Monad to wrap the blockchainaround
+-- a state (making it 'mutable')
+-- https://stackoverflow.com/questions/31952792/how-do-i-use-a-persistent-state-monad-with-spock
+restartableStateT :: s -> IO (RunStateT s IO)
+restartableStateT s0 = do
+    r <- newIORef s0
+    return $ RunStateT $ \act -> do
+        s <- readIORef r
+        (x, s') <- S.runStateT act s
+        atomicModifyIORef' r $ const (s', x)
 
 
 main :: IO ()
 main = do
-    print genesisBlock
-    print $ hashBlock genesisBlock
+    runner <- restartableStateT [genesisBlock]
+    runSpock 8080 $ spockT (runStateT runner) $ do
+        get "/" $ (S.lift getBlockchain) >>= json
+        get "current" $ (S.lift current) >>= json
+        get "mine" $ (S.lift mine) >>= json
+        get "block" $ do
+            blockid <- param "blockid"
+            case blockid of
+              (Just h) -> (S.lift $ getBlockhash h) >>= json
+              _        -> text "please supply blockid"
+        post "addtx" $ do
+            from <- param "from"
+            to <- param "to"
+            amount <- param "amount"
+            case (from, to, amount) of
+              (Just f, Just t, Just a) -> (S.lift $ addTx (Transaction f t a)) >>= json
+              _                        -> text "missing/wrong params fam"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,32 +1,45 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib where
 
 import qualified Crypto.Hash.SHA256     as SHA256
+import           Data.Aeson             hiding (json)
 import           Data.ByteString        (ByteString, append, pack)
-import           Data.ByteString.Base16 (decode, encode)
+import qualified Data.ByteString.Base16 as Base16
 import           Data.ByteString.UTF8   (fromString, toString)
 import qualified Data.Word
+import           GHC.Generics
 import           Unsafe.Coerce          (unsafeCoerce)
 
 data Transaction = Transaction { from   :: String
                                , to     :: String
                                , amount :: Float
-                               } deriving (Show)
+                               } deriving (Generic, Show)
 
 data Block = Block { index    :: Int  -- Index of the block
                    , txs      :: [Transaction] -- List of transactions in the block
-                   , hash     :: Data.ByteString.ByteString -- Hash of the block
-                   , prevHash :: Data.ByteString.ByteString -- Prev Hash of the block
+                   , hash     :: String -- Hash of the block
+                   , prevHash :: String -- Prev Hash of the block
                    , nonce    :: Maybe Int -- Nonce of the block (proof of work)
-                   } deriving (Show)
+                   } deriving (Generic, Show)
+
+instance ToJSON Transaction
+
+instance FromJSON Transaction
+
+instance ToJSON Block
+
+instance FromJSON Block
 
 -- SHA256 our Block
 -- Blockhash is dependent on current block and prev block hash
-hashBlock :: Block -> ByteString
-hashBlock (Block blockIndex blockTxs blockHash prevHash _) = encode digest
+--
+hashBlock :: Block -> String
+hashBlock (Block blockIndex blockTxs blockHash prevHash _) = toString $ Base16.encode digest
     where blockTxStr = foldr ((++) . show) "" blockTxs
-          blockString = blockTxStr ++ toString blockHash
-          blockByteStr = fromString blockString
-          ctx = SHA256.updates SHA256.init [blockByteStr, prevHash]
+          ctx = SHA256.updates SHA256.init $ fmap fromString [blockTxStr, prevHash]
           digest = SHA256.finalize ctx
 
 -- Adds a transaction to the head of the block
@@ -37,13 +50,12 @@ blockAddTx (Block i ts h p n) t = Block i (ts ++ [t]) h p n
 -- Mine the block
 -- sha256 of the hashed block starts with a 0
 mineBlock :: Block -> Int -> Block
-mineBlock b@(Block i t h p _) n = case head pow of
-                                    '0' -> Block i t h p (Just n)
+mineBlock b@(Block i t _ p _) n = case head pow of
+                                    '0' -> Block i t blockHash p (Just n)
                                     _   -> mineBlock b (n + 1)
     where blockHash = hashBlock b
-          nonce = fromString $ show n
-          ctx = SHA256.updates SHA256.init [blockHash, nonce]
-          pow = toString . encode $ SHA256.finalize ctx
+          ctx = SHA256.updates SHA256.init (fmap fromString [blockHash, show n, p])
+          pow = toString . Base16.encode $ SHA256.finalize ctx -- proof of work
 
 
 -- Our genesis block
@@ -52,5 +64,5 @@ genesisBlock :: Block
 genesisBlock = Block blockIndex blockTxs blockHash prevHash Nothing
     where blockIndex = 0
           blockTxs = [Transaction "heaven" "kendrick" 15]
-          blockHash = encode $ fromString "000000000000000000000000000000001"
-          prevHash = encode $ fromString "000000000000000000000000000000000"
+          blockHash = ""
+          prevHash = "000000000000000000000000000000000"
